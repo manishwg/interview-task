6713bfc71fd8437c51f069ecf410adb86ba6c1df
manishwg <manishwg@gmail.com>
2019-08-01 23:58:36 +0530

    requirements added

--- track.py
+++ track.py
@@ -8,6 +8,9 @@ from texttable import Texttable
 import json
 from datetime import datetime
 
+session = requests.session()
+session.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36'})
+
 # To print data list in tabular format
 def printTable(data):
 	t = Texttable(max_width=140)
@@ -15,19 +18,25 @@ def printTable(data):
 	# t.set_cols_width([20,30])
 	print(t.draw())
 
+def showHelp(message):
+	print(message)
+	print("\tUses: $ python track.py <client-name> <tracking-number>")
+	print("\tsupported client names: directlog, ninja_van_thailand")
 
+
+# directlog parcel parsing
 def track_directlog(trackid):
-	global s
+	global session
 	track_info = dict()
 	track_info['stat'] = 0
 
 	tknParser = '//input[@id="tknConsulta2"]/@value'
 	try:
-		r = s.get("https://www.directlog.com.br/")
-		if r.status_code == 200:
+		response = session.get("https://www.directlog.com.br/")
+		if response.status_code == 200:
 
 			track_info['stat'] = 1
-			tree = html.fromstring(r.content)
+			tree = html.fromstring(response.content)
 
 			tkn = tree.xpath(tknParser)
 
@@ -38,37 +47,31 @@ def track_directlog(trackid):
 						'tknConsulta': tkn[0] } 
 	  
 				# sending post request and saving response as response object 
-				r = s.post(url = 'https://www.directlog.com.br/track_individual/index.asp', data = data) 
+				response = session.post(url = 'https://www.directlog.com.br/track_individual/index.asp', data = data) 
 				  
 
-				if r.status_code == 200:
+				if response.status_code == 200:
 
-					if 'erro_individual.html' in str(r.content):
+					if 'erro_individual.html' in str(response.content):
 						print('invalid tracking nuber')
 						return
-
-					soup = BeautifulSoup(r.content, 'html.parser')
+					# parsing stuff
+					soup = BeautifulSoup(response.content, 'html.parser')
 					tbl = soup.findAll('table', attrs = {'width':'95%'})
 					trs = tbl[2].select('tr', recursive=False)
 
-					parseData = []
-					ls = []
+					ls, parseData = [], []
 
 					for td in trs[1].select('td', recursive=False):
 						ls.append(td.text)
 					parseData.append(ls)
 
 					for tr in trs[2].select('tr', recursive=False):
-						# print('\n\n')
-						# print(type(tr))
-						ls = []
-
-						tds = tr.findAll('td')
+						ls, tds = [], tr.findAll('td')
 						if len(tds)>=5:
 							for td in tds:
 								ls.append(td.text)
 							parseData.append(ls)
-					# print(parseData)
 					printTable(parseData)
 			else:
 				print("[Error]: Unable to get token")
@@ -82,16 +85,16 @@ def track_directlog(trackid):
 	    sys.exit(1)
 	return
 
-
+# ninjavan parcel parse
 def track_ninjavan(trackid):
-	global s
+	global session
 	try:
-		r = s.get("https://api.ninjavan.co/th/shipperpanel/app/tracking?&id=" + trackid)
+		response = session.get("https://api.ninjavan.co/th/shipperpanel/app/tracking?&id=" + trackid)
 
-		if r.status_code == 200:
+		if response.status_code == 200:
 			parseData = [['time','description']]
-			# print(r.content)
-			data = json.loads(r.content)
+			# print(response.content)
+			data = json.loads(response.content)
 			event = data['orders'][0]['events']
 			for i in event:
 				# print(type(i['time']))
@@ -99,7 +102,7 @@ def track_ninjavan(trackid):
 			# print(parseData)
 			printTable(parseData)
 
-		elif r.status_code == 404:
+		elif response.status_code == 404:
 			print( 'invalid tracking number' ) 
 
 	except requests.exceptions.Timeout:
@@ -111,18 +114,17 @@ def track_ninjavan(trackid):
 	    sys.exit(1)
 
 
-
-s = requests.session()
-s.headers.update({'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.87 Safari/537.36'})
-
 # Dict of input : function_name
 client_dict = {
 			'directlog': track_directlog,
 			'ninja_van_thailand': track_ninjavan
 			}
-
-if sys.argv[1] in client_dict.keys():
-	client_dict[sys.argv[1]](sys.argv[2])
+if len(sys.argv) is 3:
+	if sys.argv[1] in client_dict.keys():
+		client_dict[sys.argv[1]](sys.argv[2])
+	else:
+		showHelp('invalid argument')
 else:
-	print('invalid argument')
-s.close()
+	showHelp("insufficient parameters")
+
+session.close()
